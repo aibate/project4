e2c5a9390ce61a5588d00d1cffb1210c4d8f3e31
aibate <indigoambe@gmail.com>
2022-10-17 18:53:15 +1100

    set up the repository

---
+++ server.js
@@ -0,0 +1,15 @@
+const { application } = require('express');
+const express = require('express');
+const app = express();
+
+
+const port = process.env.PORT || 3001;
+
+
+
+app.use(express.json());
+app.use(express.static('./client/build'))
+
+app.get("/api/test", (req, res)=> res.json({result: "ok"}));
+
+app.listen(port, ()=>console.log(`Listening at localhost:${port}`));
