039db49c63dc320b7f497c28b4a857323ec4ae7f
aibate <indigoambe@gmail.com>
2022-10-17 21:09:07 +1100

    Changed server.js for react router

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const { application } = require('express');
 const express = require('express');
+const fs = require('fs');
+const { request } = require('http');
 const app = express();
 
 
@@ -11,5 +13,11 @@ app.use(express.json());
 app.use(express.static('./client/build'))
 
 app.get("/api/test", (req, res)=> res.json({result: "ok"}));
+
+app.get('*', (req, res)=> {
+    request.setHeader('Content-Type', 'text/html');
+    fs.createReadStream(`${__dirname}/client/build/index.html`).pipe(res);
+})
+
 
 app.listen(port, ()=>console.log(`Listening at localhost:${port}`));
